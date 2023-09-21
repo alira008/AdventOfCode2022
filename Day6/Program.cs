051aa3f54e97486995cb683aeec77353312183e4
Ariel Lira <ariellira.al@gmail.com>
2023-09-21 08:51:35 -0700

    finished day 6

---
+++ Day6/Program.cs
@@ -0,0 +1,38 @@
+﻿string ReadFile(string filename)
+{
+    return System.IO.File.ReadAllText(filename);
+}
+
+void Solve(string filename, int amountOfDistinctChars)
+{
+    var input = ReadFile(filename);
+
+    for (int i = 0; i < input.Length - 4; ++i)
+    {
+        System.Collections.Generic.HashSet<char> set = new();
+
+        int j = i;
+        var isFound = true;
+        for (; j < i + amountOfDistinctChars && j < input.Length; ++j)
+        {
+            if (!set.Contains(input[j]))
+            {
+                set.Add(input[j]);
+            }
+            else
+            {
+                isFound = false;
+                break;
+            }
+        }
+        if (isFound)
+        {
+            Console.WriteLine(j);
+            break;
+        }
+    }
+}
+
+Solve("input_one.txt", 4);
+
+Solve("input_two.txt", 14);
