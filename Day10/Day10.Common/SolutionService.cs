01270cb0d25e9ad3bff0570f9e75448a45df6be6
Ariel Lira <ariellira.al@gmail.com>
2023-11-30 14:17:45 -0800

    started working on Day10

--- Day10/Day10.Common/SolutionService.cs
+++ Day10/Day10.Common/SolutionService.cs
@@ -48,4 +48,122 @@ public class SolutionService
 
         return signalStrengths.Sum();
     }
+
+    class Location(int x, int y)
+    {
+        public int X { get; set; } = x;
+        public int Y { get; set; } = y;
+    }
+
+    class Sprite
+    {
+        public Location Left { get; set; } = new(0, 0);
+        public Location Middle { get; set; } = new(1, 0);
+        public Location Right { get; set; } = new(2, 0);
+    }
+
+    const int WIDTH = 40;
+    const int HEIGHT = 6;
+
+    private void MoveSprite(Sprite sprite, int moveAmount)
+    {
+        if (sprite.Right.X + moveAmount < WIDTH - 1)
+        {
+            sprite.Left.X += moveAmount;
+            sprite.Middle.X += moveAmount;
+            sprite.Right.X += moveAmount;
+        }
+        else if (sprite.Right.X + moveAmount >= WIDTH - 1)
+        {
+            sprite.Left.X = WIDTH - 1 - moveAmount;
+            sprite.Middle.X = WIDTH - 1 - moveAmount;
+            sprite.Right.X = WIDTH - 1 - moveAmount;
+            sprite.Left.Y += 1;
+            sprite.Middle.Y += 1;
+            sprite.Right.Y += 1;
+        }
+        else if (sprite.Left.X + moveAmount < 0)
+        {
+            sprite.Left.X = WIDTH - 1 + (moveAmount - sprite.Left.X);
+            sprite.Middle.X = WIDTH - 1 + (moveAmount - sprite.Middle.X);
+            sprite.Right.X = WIDTH - 1 + (moveAmount - sprite.Right.X);
+            sprite.Left.Y += 1;
+            sprite.Middle.Y += 1;
+            sprite.Right.Y += 1;
+        }
+    }
+
+    private void PrintChar(ref int i, ref int j, List<List<char>> screen, char c)
+    {
+        if (i < WIDTH - 2)
+        {
+            screen[j].Add(c);
+            i += 1;
+        }
+        else if (i >= WIDTH - 2)
+        {
+            screen[j].Add(c);
+            i = 0;
+            j += 1;
+            screen.Add(new List<char>());
+        }
+    }
+
+    private void PrintCrt(ref int i, ref int j, List<List<char>> screen, Sprite sprite)
+    {
+        if (CompareSpriteLocationScreenLocation(i, j, sprite))
+        {
+            PrintChar(ref i, ref j, screen, '#');
+        }
+        // if sprite location and screen location are not the same print .
+        else
+        {
+            PrintChar(ref i, ref j, screen, '.');
+        }
+    }
+
+    private bool CompareSpriteLocationScreenLocation(int i, int j, Sprite sprite)
+    {
+        return (sprite.Right.X == i && sprite.Right.Y == j)
+            || (sprite.Middle.X == i && sprite.Middle.Y == j)
+            || (sprite.Left.X == i && sprite.Left.Y == j);
+    }
+
+    public List<List<char>> PartTwo(string input)
+    {
+        var lines = input.Split("\n").Select(line => line.Trim()).ToList();
+
+        Sprite sprite = new();
+        List<List<char>> screen = new();
+        screen.Add(new List<char>());
+        int iHeight = 0,
+            jWidth = 0;
+
+        foreach (var line in lines)
+        {
+            if (line.Contains("noop"))
+            {
+                // if sprite location and screen location are the same print #
+                PrintCrt(ref iHeight, ref jWidth, screen, sprite);
+            }
+            else if (line.Contains("addx"))
+            {
+                int num = line.Split(" ")
+                    .Where(tok => int.TryParse(tok, out _))
+                    .Select(tok => int.Parse(tok))
+                    .ToList()
+                    .First();
+
+                // two cycles
+                // if sprite location and screen location are the same print #
+                PrintCrt(ref iHeight, ref jWidth, screen, sprite);
+
+                // does not change until after second cycle
+                PrintCrt(ref iHeight, ref jWidth, screen, sprite);
+                MoveSprite(sprite, num);
+            }
+        }
+
+        return screen;
+    }
 }
--- Day10/Day10.Testing/UnitTest1.cs
+++ Day10/Day10.Testing/UnitTest1.cs
@@ -7,7 +7,8 @@ public class UnitTest1
     {
         var solutionService = new Day10.Common.SolutionService();
 
-        var input = @"addx 15
+        var input =
+            @"addx 15
 addx -11
 addx 6
 addx -3
@@ -157,5 +158,164 @@ noop";
         var answer = solutionService.PartOne(input);
 
         Assert.Equal(13140, answer);
+    }
+
+    [Fact]
+    void PartTwo()
+    {
+        var solutionService = new Day10.Common.SolutionService();
+
+        var input =
+            @"addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop";
+
+        var answer = solutionService.PartTwo(input);
+        List<List<char>> screen = [['#', '#', '.', '.', '#', '#', '.', '.', '#', '#', '.', '.', '#', '#', '.', '.', '#', '#', '.', '.', '#', '#', '.', '.', '#', '#', '.', '.', '#', '#', '.', '.', '#', '#', '.', '.', '#', '#', '.', '.'], ['#', '#', '#', '.', '.', '.', '#', '#', '#', '.', '.', '.', '#', '#', '#', '.', '.', '.', '#', '#', '#', '.', '.', '.', '#', '#', '#', '.', '.', '.', '#', '#', '#', '.', '.', '.', '#', '#', '#', '.', '.', '.'], ['#','#','#','#', '.', '.', '.', '.', '#','#','#','#', '.', '.', '.', '.', '#','#','#','#', '.', '.', '.', '.', '#','#','#','#', '.', '.', '.', '.', '#','#','#','#', '.', '.', '.', '.'], ['#', '#', '#', '#', '#', '.', '.', '.', '.', '.', '#', '#', '#', '#', '#', '.', '.', '.', '.', '.', '#', '#', '#', '#', '#', '.', '.', '.', '.', '.', '#', '#', '#', '#', '#', '.', '.', '.', '.', '.'], ['#', '#', '#', '#', '#', '#', '.', '.', '.', '.', '.', '.', '#', '#', '#', '#', '#', '#', '.', '.', '.', '.', '.', '.', '#', '#', '#', '#', '#', '#', '.', '.', '.', '.', '.', '.', '#', '#', '#', '#' ], ['#', '#', '#', '#', '#', '#', '#', '.', '.', '.', '.', '.', '.', '.', '#', '#', '#', '#', '#', '#', '#', '.', '.', '.', '.', '.', '.', '.', '#', '#', '#', '#', '#', '#', '#', '.', '.', '.', '.']];
+
+        Assert.Equal(screen, answer);
     }
 }
